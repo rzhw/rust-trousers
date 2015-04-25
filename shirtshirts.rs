18436bc6186d1a0c3fa6043c5a578615fc803ca1
Richard Wang <zhwang@live.com.au>
2015-04-25 20:10:54 +1000

    Implement Tspi_Context_Connect, Tspi_Context_GetTpmObject

--- shirtshirts.rs
+++ shirtshirts.rs
@@ -1,14 +1,20 @@
+use std::ptr;
+
 struct TssContext { handle: u32 }
 struct TssTpm { handle: u32 }
 
 type TssHContext = u32;
+type TssHTPM = u32;
 type TssResult = u32;
+type TssUnicode = u16;
 
 const TSS_SUCCESS: TssResult = 0;
 
 #[link(name = "tspi")]
 extern {
     fn Tspi_Context_Create(phContext: *mut TssHContext) -> TssResult;
+    fn Tspi_Context_Connect(phContext: TssHContext, wszDestination: *const TssUnicode) -> TssResult;
+    fn Tspi_Context_GetTpmObject(phContext: TssHContext, phTPM: *mut TssHTPM) -> TssResult;
     fn Tspi_Context_Close(phContext: TssHContext) -> TssResult;
 }
 
@@ -17,10 +23,34 @@ impl TssContext {
         unsafe {
             let mut handle = 0;
             let result = Tspi_Context_Create(&mut handle);
-            if result == TSS_SUCCESS {
-                Ok(TssContext { handle: handle })
-            } else {
+            if result != TSS_SUCCESS {
                 Err(result)
+            } else {
+                Ok(TssContext { handle: handle })
+            }
+        }
+    }
+
+    // TODO: support destination
+    fn connect(&self) -> Result<(), TssResult> {
+        unsafe {
+            let result = Tspi_Context_Connect(self.handle, ptr::null());
+            if result != TSS_SUCCESS {
+                Err(result)
+            } else {
+                Ok(())
+            }
+        }
+    }
+
+    fn get_tpm_object(&self) -> Result<TssTpm, TssResult> {
+        unsafe {
+            let mut handle = 0;
+            let result = Tspi_Context_GetTpmObject(self.handle, &mut handle);
+            if result != TSS_SUCCESS {
+                Err(result)
+            } else {
+                Ok(TssTpm { handle: handle })
             }
         }
     }
@@ -35,10 +65,27 @@ impl Drop for TssContext {
 }
 
 fn main() {
-    let blah = TssContext::new();
+    // TODO: Any cleaner way to write this?
+    let contextresult = TssContext::new();
+    if let Ok(context) = contextresult {
+        if let Ok(_) = context.connect() {
+            if let Ok(tpm) = context.get_tpm_object() {
+                println!("Success! TPM handle: {:?}", tpm.handle);
+            } else {
+                println!("Failed to get TPM handle :(")
+            }
+        } else {
+            println!("Failed to connect :(");
+        }
+    } else {
+        println!("Failed :(");
+    }
+
+    /*
     match blah {
         Ok(context) => println!("Context created! {:?}", context.handle),
         Err(e) => println!("Context failed with err {:?}", e),
     }
     println!("Hello world!");
+    */
 }
